261255dfcd7749012dd60c8a44328a5deda30cd2
sudiptabhatta <sudiptabhatta05@gmail.com>
2024-04-09 16:33:19 +0600

    add: getting, updating, deleting current user book photo api implementation

--- server/book_browse/urls.py
+++ server/book_browse/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('photo/upload/', views.BookPhotoUploadView.as_view(), name='book-photo-upload'),
     path('timeline/', views.Timeline.as_view(), name='timeline'),
+    path('<int:book_id>/', views.CurrentUserBookPhotoRetrieveUpdateDeleteView.as_view(), name='bookphoto-retrieve-update-delete'),
 ]
--- server/book_browse/views.py
+++ server/book_browse/views.py
@@ -6,6 +6,9 @@ from rest_framework.views import APIView
 from .serializers import BookSerializer, CurrentUserBookPhotosSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from .models import Book
+
 
 # Create your views here.
 class BookPhotoUploadView(APIView):
@@ -45,8 +48,47 @@ class Timeline(APIView):
 
         serializer = CurrentUserBookPhotosSerializer(instance=user)
 
-        print(serializer.data)
+        # print(serializer.data)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
+
+
+class CurrentUserBookPhotoRetrieveUpdateDeleteView(APIView):
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request: Request, book_id: int):
+        book_photo = get_object_or_404(Book, pk=book_id)
+
+        serializer = self.serializer_class(instance=book_photo)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+
+    def put(self, request: Request, book_id: int):
+        book_photo = get_object_or_404(Book, pk=book_id) 
+
+        data = request.data 
+
+        serializer = self.serializer_class(instance=book_photo, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            response = {
+                "message": "Book Photo Updated Successfully.",
+                "data": serializer.data 
+            }
+
+            return Response(data=response, status=status.HTTP_200_OK)
+        
+        return Response(data={"error": "Please Select a Book Photo to Upload."}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request: Request, book_id: int):
+        book_photo = get_object_or_404(Book, pk=book_id) 
+
+        book_photo.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT) 
